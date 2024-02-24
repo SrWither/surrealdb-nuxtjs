1b42823af71768cc4967d1999edad234f9685c4b
SrWither <fjachuf@gmail.com>
2024-02-23 21:24:48 -0300

    post style

--- pages/[id].vue
+++ pages/[id].vue
@@ -6,11 +6,15 @@ const { data } = await useFetch(`/api/posts/${postId}`)
 
 <template>
   <div>
-    <NuxtLink to="/">Back</NuxtLink>
-    <h1>Post: </h1>
-    <h1>{{ data.id }}</h1>
-    <h1>{{ data.title }}</h1>
-    <h1>{{ data.description }}</h1>
-    <h1>{{ data.content }}</h1>
+    <div class="flex flex-col justify-center items-center space-y-4 pt-2">
+      <NuxtLink to="/">
+        <Button label="Regresar" />
+      </NuxtLink>
+      <h1 class="font-bold text-4xl">{{ data.title }}</h1>
+      <h4 class="text-lg">{{ data.description }}</h4>
+    </div>
+    <div class="m-4">
+      <p>{{ data.content }}</p>
+    </div>
   </div>
 </template>
